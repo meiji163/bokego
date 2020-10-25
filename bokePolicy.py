7dcdbd1b80b872fd90797ee5012a5c70520bf6d8
meiji163 <mysatellite99@gmail.com>
2020-10-25 13:48:38 -0700

    rewrote dataloader

--- bokePolicy.py
+++ bokePolicy.py
@@ -1,31 +1,33 @@
 import go
-from parse_sgf import get_moves
 import os
 import numpy as np
 import torch
+from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import argparse
 
-class Policy(nn.Module):
+class PolicyNet(nn.Module):
     def __init__(self):
-        super(Policy, self).__init__()
-        # 3x3 Convolution: 9x9 --> 5x5x(30 features) --> 3x3x30
-        # + 3 fully connected layers
-        self.conv = nn.Conv2d(1,30,3)
-        self.pool = nn.MaxPool2d((2,2))
-        self.l1 = nn.Linear(30*3*3, 200)
-        self.l2 = nn.Linear(200, 120)
-        self.l3 = nn.Linear(120, 81)
+        super(PolicyNet, self).__init__()
+        '''7 9x9 input features
+        5x5 convolution 9x9 -> 9x9
+        3x3 convolution 9x9 -> 7x7
+        1 fully connected hidden layer
+        output distribution over coords 0-81'''
+        self.conv1 = nn.Conv2d(7, 10, 5, padding = 2)
+        self.conv2 = nn.Conv2d(10, 15, 3) 
+        self.l1 = nn.Linear(15*7*7, 200)
+        self.l2 = nn.Linear(200, 81)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv(x)))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = self.l3(x)
-        return x
+        x = self.l2(x)
+        return F.softmax(x, dim = 1)
 
     def num_flat_features(self, x):
         size = x.size()[1:]
@@ -34,25 +36,101 @@ class Policy(nn.Module):
             num_features *= s
         return num_features
 
-def policy_predict(policy, board, device = "cpu"):
-    board = np.array(board).reshape((9,9))
-    board = torch.Tensor(board).unsqueeze(0).unsqueeze(0).float()
-    predicts = torch.topk(policy(board).squeeze(0), 3)
-    return [go.unsquash(sq_c) for sq_c in predicts[1].tolist()]
+class NinebyNineGames(Dataset):
+    def __init__(self, root_dir):
+        '''Read all the sgf files from root_dir'''
+        self.root_dir = root_dir
+        self.sgf_files = [entry for entry in os.scandir(self.root_dir) if entry.path.endswith(".sgf")]
+        self.games = [get_moves(sgf) for sgf in self.sgf_files]
+        #stores previously computed game states
+        #__cache[n] stores board states from game n move i as a dict {i:(board, ko)}
+        self.cache = {} 
 
-if __name__ == "__main__":
+    def __len__(self):
+        return sum( len(game) for game in self.games)
+
+
+    def __getitem__(self, idx):
+        g_idx = 0
+        count = 0
+        while(idx > count + len(self.games[g_idx]) -1 ) :
+            count += len(self.games[g_idx])
+            g_idx += 1
+        if g_idx in self.cache and self.cache[g_idx]:
+            c_boards = self.cache[g_idx] 
+            c_idx = max([i for i in c_boards if i<= idx-count])
+            board, ko = c_boards[c_idx]
+            g = go.Game(board = board, turn = c_idx, ko = ko, moves = self.games[g_idx])
+        else:
+            self.cache[g_idx] = {}
+            g = go.Game( moves = self.games[g_idx])
+            c_idx = 0
+        for i in range(idx-count-c_idx):
+                g.play_move()
+                self.cache[g_idx][c_idx+i+1] = g.board, g.ko
+
+        board, move =  np.array(g.get_board()).reshape(9,9) , g.moves[idx - count]
+        return torch.Tensor(features(g, 10)) , go.squash(move)
+
+def features(game: go.Game, scale = 1):
+    ''' layer : feature
+        0: black stones
+        1: white stones
+        2: empty 
+        4: legal
+        5: liberties
+        6: liberties after playing'''
+    empty = np.array(game.get_board()).reshape(9,9)
+    black = empty.copy()
+    white = empty.copy()
+    black[black == -1] = 0
+    white[white == 1] = 0
+    white *= -1
+    empty[empty == 0] = 2 
+    empty[empty != 2] = 0
+    legal = np.array([game.is_legal((i,j)) for i in range(9) for j in range(9)]).reshape(9,9)
+    libs = np.array(game.get_liberties()).reshape(9,9)
+    color = (go.BLACK if game.turn%2 == 0 else go.WHITE)
+    libs_after = np.array([go.get_stone_lib(go.place_stone(color, game.board,\
+            go.squash((i,j))), (i,j)) for i in range(9) for j in range(9)]).reshape(9,9)
+    return scale*np.stack((black, white, empty, legal, libs, libs_after)) 
+
+def get_moves(sgf):
+    with open(sgf, 'r') as f:
+        match = re.findall(r"[BW]\[(\w*)\]", f.read())
+    mvs = []
+    for mv in match:
+        if len(mv)!= 2:
+            break
+        else: 
+            mvs.append((ord(mv[0])-97, ord(mv[1])-97) )
+    return mvs
+
+def get_result(sgf):
+    with open(sgf, 'r') as f:
+        match = re.match(r"RE\[()\]")
+    if match:
+        return match.group(1)
+    else:
+        return "No result"
+ 
+def policy_predict(policy: PolicyNet, game: go.Game , device = "cpu"):
+    fts = torch.Tensor(features(game, 10)).unsqueeze(0)
+    predicts = torch.topk(policy(fts).squeeze(0), 5)
+    return {go.unsquash(sq_c): predicts[0].item for sq_c in predicts[1].tolist()}
+
+if  __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "BokeGo Policy Prediction")
-    parser.add_argument("path", metavar="path", type = str, nargs = 1, help = "path to model")
-    parser.add_argument("--sgf", metavar="sgf", type = str, nargs = 1, help = "path to sgf")
+    parser.add_argument("path", metavar="MODEL", type = str, nargs = 1, help = "path to model")
+    parser.add_argument("--sgf", metavar="SGF", type = str, nargs = 1, help = "path to sgf")
     args = parser.parse_args()
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    pi = Policy()
+    pi = PolicyNet()
     pi.load_state_dict(torch.load(args.path[0], map_location=device))
     pi.eval()
     
     if args.sgf != None:
-        print(args.sgf)
         mvs = get_moves(args.sgf[0])
         g = go.Game(moves = mvs)
 
@@ -66,7 +144,7 @@ if __name__ == "__main__":
         - enter coordinate to play move\n\
         - press q to quit\n")
         if uin == 'p':
-            print(policy_predict(pi, g.get_board(), device))
+            print(policy_predict(pi, g, device))
         else:
             g.play_move(tuple([int(i) for i in uin.split(' ')]))
 
--- go.py
+++ go.py
@@ -4,25 +4,105 @@ N = 9
 WHITE, BLACK, EMPTY = 'O', 'X', '.'
 EMPTY_BOARD = EMPTY*(N**2) 
 
-def swap_colors(color):
-    if color == BLACK:
-        return WHITE
-    elif color == WHITE:
-        return BLACK
-    else:
-        return color
+class Game():
+    def __init__(self, board = EMPTY_BOARD, ko = None, turn = 0, moves = []):
+        self.turn = turn
+        self.ko = ko
+        self.board= board
+        self.moves = moves
 
-def turn_color(turn):
-    if turn%2 == 0:
-        return BLACK
-    return WHITE
+    def get_board(self):
+        '''return board as array'''
+        enc = {BLACK: 1, WHITE: -1, EMPTY: 0}
+        return [enc[s] for s in self.board]
 
+    def __str__(self):
+        return '\n'.join(wrap(self.board, N))
+
+    def play_move(self, c = None, testing = False):
+        '''play move from self.moves. If a coordinate is given that is played instead.'''
+        if c == None:
+            if self.turn >= len(self.moves):
+                print("No more moves to play.")
+                return
+            c = self.moves[self.turn]
+        sq_c = squash(c)
+        
+        if sq_c == self.ko:
+            raise IllegalMove(f"\n{self}\n Move at {c} illegally retakes ko.")
+        if self.board[sq_c] != EMPTY:
+            raise IllegalMove(f"\n{self}\n There is already a stone at {c}")
+        color = (WHITE if self.turn%2 ==1 else BLACK) 
+        possible_ko_color = possible_ko(self.board, sq_c)
+        new_board = place_stone(color, self.board, sq_c)
+
+        opp_color = (BLACK if color == WHITE else WHITE) 
+        opp_stones = []
+        my_stones = []
+        for sq_n in NEIGHBORS[sq_c]:
+            if new_board[sq_n] == color:
+                my_stones.append(sq_n)
+            elif new_board[sq_n] == opp_color:
+                opp_stones.append(sq_n)
+
+        opp_captured = []
+        for sq_s in opp_stones:
+            new_board, captured = maybe_capture_stones(new_board, sq_s)
+            opp_captured += captured
+
+        # Check for suicide
+        new_board, captured = maybe_capture_stones(new_board, sq_c)
+        if captured:
+            raise IllegalMove(" \n{self}\n Move at {c} is suicide.")
+
+        if len(opp_captured) == 1 and possible_ko_color == opp_color:
+            new_ko = opp_captured[0] 
+        else:
+            new_ko = None
+
+        if not testing: 
+            self.board = new_board
+            self.ko = new_ko
+            self.turn += 1 
+
+    def is_legal(self, c):
+        try:
+            self.play_move(c, testing = True)
+            return True
+        except IllegalMove:
+            return False
+
+    def score(self):
+        '''returns Bs score minus Ws score'''
+        board = self.board
+        while EMPTY in board:
+            empty = board.index(EMPTY)
+            empties, borders = flood_fill(board, empty)
+            possible_border_color = board[list(borders)[0]]
+            if all(board[sq_b] == possible_border_color for sq_b in borders):
+                board = bulk_place_stones(possible_border_color, board, empties)
+            else:
+                board = bulk_place_stones('?', board, empties)
+        return board.count(BLACK) - board.count(WHITE)
+
+    def get_liberties(self):
+        board = self.board
+        liberties = bytearray(N*N)
+        for color in (WHITE, BLACK):
+            while color in board:
+                sq_c = board.index(color)
+                stones, borders = flood_fill(board, sq_c)
+                num_libs = len([sq_b for sq_b in borders if board[sq_b] == EMPTY])
+                for sq_s in stones:
+                    liberties[sq_s] = num_libs
+                board = bulk_place_stones('?', board, stones)
+        return list(liberties)
+
+#squash converts coordinate pair 0 <= x,y < N  to single integer 0 <= n < N^2
 def squash(c):
-    '''convert (x,y) coordinate to squashed coordinate in {0,..., N**2-1} '''
     return N * c[0] + c[1]
 
 def unsquash(sq_c):
-    '''convert squashed coordinate sq_c to coordinate c'''
     return divmod(sq_c, N)
 
 def is_on_board(c):
@@ -35,7 +115,8 @@ def get_valid_neighbors(sq_c):
 
 NEIGHBORS = [get_valid_neighbors(sq_c) for sq_c in range(N*N)]
 
-def find_reached(board, sq_c):
+def flood_fill(board, sq_c):
+    '''Flood fill to find the connected component containing sq_c and its boundary'''
     color = board[sq_c]
     chain = set([sq_c])
     reached = set()
@@ -50,8 +131,14 @@ def find_reached(board, sq_c):
                 reached.add(sq_n)
     return chain, reached
 
+def get_stone_lib(board, c):
+    sq_c = squash(c)
+    stones, borders = flood_fill(board, sq_c)
+    return len([sq_b for sq_b in borders if board[sq_b] == EMPTY])
+
 class IllegalMove(Exception): pass
 
+#Helper functions
 def place_stone(color, board, sq_c):
     return board[:sq_c] + color + board[sq_c+1:]
 
@@ -63,7 +150,7 @@ def bulk_place_stones(color, board, stones):
     return byteboard.decode('ascii') 
 
 def maybe_capture_stones(board, sq_c):
-    chain, reached = find_reached(board, sq_c)
+    chain, reached = flood_fill(board, sq_c)
     if not any(board[sq_r] == EMPTY for sq_r in reached):
         board = bulk_place_stones(EMPTY, board, chain)
         return board, chain
@@ -75,7 +162,7 @@ def play_move_incomplete(board, sq_c, color):
         raise IllegalMove
     board = place_stone(color, board, sq_c)
 
-    opp_color = swap_colors(color)
+    opp_color = WHITE if color == BLACK else WHITE
     opp_stones = []
     my_stones = []
     for sq_n in NEIGHBORS[sq_c]:
@@ -100,95 +187,4 @@ def possible_ko(board, sq_c):
         return list(neighbor_colors)[0]
     else:
         return None
-
-class Game():
-    def __init__(self, board = EMPTY_BOARD, ko = None, turn = 0, moves = []):
-        self.turn = turn
-        self.ko = ko
-        self.board= board
-        self.moves = moves
-
-    def get_board(self):
-        '''return board as array'''
-        enc = {BLACK: 255, WHITE: -255, EMPTY: 0}
-        return [enc[s] for s in self.board]
-
-    def __str__(self):
-        return '\n'.join(wrap(self.board, N))
-    
-    def play_move(self, c = None):
-        '''play move from self.moves. If a coordinate is given that is played instead.'''
-        color = turn_color(self.turn)
-        if c != None:
-            try:
-                self.moves[self.turn] = c
-            except IndexError:
-                self.moves.append(c)
-        else:
-            if self.turn > len(self.moves):
-                print("No more moves to play.")
-            c = self.moves[self.turn]
-
-        sq_c = squash(c)
-        if sq_c == self.ko:
-            raise IllegalMove(f"{self}\n Move at %{c} illegally retakes ko.") 
-
-        if self.board[sq_c] != EMPTY:
-            raise IllegalMove(f"{self}\n Stone exists at {c}.")
-
-        possible_ko_color = possible_ko(self.board, sq_c)
-        new_board = place_stone(color, self.board, sq_c)
-
-        opp_color = swap_colors(color)
-        opp_stones = []
-        my_stones = []
-        for sq_n in NEIGHBORS[sq_c]:
-            if new_board[sq_n] == color:
-                my_stones.append(sq_n)
-            elif new_board[sq_n] == opp_color:
-                opp_stones.append(sq_n)
-
-        opp_captured = []
-        for sq_s in opp_stones:
-            new_board, captured = maybe_capture_stones(new_board, sq_s)
-            opp_captured += captured
-
-        # Check for suicide
-        new_board, captured = maybe_capture_stones(new_board, sq_c)
-        if captured:
-            raise IllegalMove("\n{self}\n Move at {c} is suicide.")
-
-        if len(opp_captured) == 1 and possible_ko_color == opp_color:
-            new_ko = opp_captured[0] 
-        else:
-            new_ko = None
-
-        self.board = new_board
-        self.ko = new_ko
-        self.turn += 1 
-
-    def score(self):
-        board = self.board
-        while EMPTY in board:
-            empty = board.index(EMPTY)
-            empties, borders = find_reached(board, empty)
-            possible_border_color = board[list(borders)[0]]
-            if all(board[sq_b] == possible_border_color for sq_b in borders):
-                board = bulk_place_stones(possible_border_color, board, empties)
-            else:
-                board = bulk_place_stones('?', board, empties)
-        return board.count(BLACK) - board.count(WHITE)
-
-    def get_liberties(self):
-        board = self.board
-        liberties = bytearray(N*N)
-        for color in (WHITE, BLACK):
-            while color in board:
-                sq_c = board.index(color)
-                stones, borders = find_reached(board, sq_c)
-                num_libs = len([fb for fb in borders if board[fb] == EMPTY])
-                for fs in stones:
-                    liberties[fs] = num_libs
-                board = bulk_place_stones('?', board, stones)
-        return list(liberties)
 
