724d3616ac2196111f6d4bb89cf2b62e151d0d57
meiji163 <mysatellite99@gmail.com>
2020-11-01 18:25:26 -0800

    modify play script

--- policy_net_py/bokePlay.py
+++ policy_net_py/bokePlay.py
@@ -35,36 +35,33 @@ if  __name__ == "__main__":
 
     while(True):
         print(g)
-        
         if args.sgf:
-            uin = input("\t- press n to see next move in sgf\n\
+            uin = input("   \t- press n to see next move in sgf\n\
         - press p to show prediction\n\
         - press q to quit\n")
         else:
-            uin = input("\t- press b to play Boke's move\n\
-        - press p to show prediction\n\
-        - enter coordinate \"x y\" to play move\n\
-        - press q to quit\n")
-
-        if not args.sgf and uin == 'b':
-            mv = policy_sample(pi,g)
-            while not g.is_legal(mv):
-                mv = policy_sample(pi,g )
-            g.play_move(policy_sample(pi, g))
-        elif args.sgf and uin == 'n':
+            uin = input("\t Enter Move: ")
+        if args.sgf and uin == 'n':
             g.play_move()
-
         elif uin == 'p':
             probs, moves = policy_predict(pi, g, device, k = 5)
             print(go.unsquash(moves.tolist()))   
             print(probs.tolist())
-        
         elif uin == 'q':
             break
-        
+        elif uin == 'u':
+            g.undo(2)
         else:
             try:
                 g.play_move(go.squash(tuple([int(i) for i in uin.split(' ') ])) )
+                mv = policy_sample(pi,g)
+                while not g.is_legal(mv):
+                    mv = policy_sample(pi,g)
+                print(g)
+                sleep(1)
+                g.play_move(policy_sample(pi, g))
             except:
-                print("Enter a valid option")
+                print("\t - Enter coordinate \"x y\" to play a move\n\
+                - Press p to show prediction\n\
+                - Press q to quit" )
 
